b49e132172ea6586b1630b8b788457970e1869f9
Nick Rutowski <nickr@doordash.com>
2017-07-01 17:33:35 -0700

    configure dev dependencies and serverside routing

--- server.jsx
+++ server.jsx
@@ -1,20 +1,36 @@
-import express from 'express';
-const app = express();
-app.use('/', (req, res) => {
-  const HTML = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <title>Isomorphic Redux Demo</title>
-    </head>
-    <body>
-      <div id="react-view"></div>
-      <script type="application/javascript" src="/bundle.js"></script>
-    </body>
-  </html>
-  `;
+import express                   from 'express';
+import React                     from 'react';
+import { renderToString }        from 'react-dom/server'
+import { StaticRouter }          from 'react-router';
+import routes                    from 'routes';
 
-  res.end(HTML);
+const app = express();
+app.get('*', (req, res) => {
+    const context = {};
+
+    const InitialComponent = (
+      <StaticRouter
+        location={req.url}
+        context={context}
+      >
+        {routes}
+      </StaticRouter>
+    );
+
+    const componentHTML = renderToString(InitialComponent);
+    const HTML = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Isomorphic Redux Demo</title>
+      </head>
+      <body>
+        <div id="react-view">${componentHTML}</div>
+        <script type="application/javascript" src="/bundle.js"></script>
+      </body>
+  </html>
+`
+    res.end(HTML);
 });
 export default app;
