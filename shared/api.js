140bc7a1923ac04b8899b4d4ca251d996d545c38
badsauce <nickrutowski@gmail.com>
2017-07-11 20:43:55 -0700

    add axios and submit to api

--- shared/api.js
+++ shared/api.js
@@ -1,3 +1,6 @@
+import axios from 'axios'
+
 export const submitPartnerEmails = (yourEmail, theirEmail) => {
+  axios.post('/api/submitPartnerEmails/',{ yourEmail, theirEmail })
   console.log(yourEmail, theirEmail)
 }
