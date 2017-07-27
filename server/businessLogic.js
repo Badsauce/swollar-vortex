d69b6089f888e16b86d0f6bd5d3064d79c8caf26
badsauce <nickrutowski@gmail.com>
2017-07-26 21:13:20 -0700

    add mailer

--- server/businessLogic.js
+++ server/businessLogic.js
@@ -1,4 +1,5 @@
 import { debugDatabase, addCouple, createList } from './dataLayer'
+import { sendWelcomeEmail } from './mailer'
 
 export const enrollCouple = (email1, email2) => {
   const uniqueCode = `${email1}${email2}`
@@ -8,5 +9,5 @@ export const enrollCouple = (email1, email2) => {
   createList(email1)
   createList(email2)
 
-  debugDatabase()
+  sendWelcomeEmail()
 }
--- server/mailer.js
+++ server/mailer.js
@@ -1,1 +1,27 @@
-console.log(process.env.EMAIL_ACCOUNT)
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: process.env.EMAIL_ACCOUNT,
+        pass: process.env.EMAIL_PASSWORD,
+    }
+})
+
+export const sendWelcomeEmail = (email) => {
+  const mailOptions = {
+    from: 'kinkmailer@gmail.com', // sender address
+    to: 'nickrutowski@gmail.com', // list of receivers
+    subject: 'Email Example', // Subject line
+    text: 'kinky timez' //, // plaintext body
+    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if(error){
+        console.log(error)
+    }else{
+        console.log('Message sent: ' + info.response)
+    }
+  })
+}
